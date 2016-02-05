3bc6394bb1ccc3e42ad4afeda332a3da4c9f4485
Sami Kallio <sami.kallio@gofore.com>
2016-02-05 11:11:20 +0200

    Transform soap headers from request to response

--- xroad-catalog-lister/src/main/java/fi/vrk/xroad/catalog/lister/ServiceEndpoint.java
+++ xroad-catalog-lister/src/main/java/fi/vrk/xroad/catalog/lister/ServiceEndpoint.java
@@ -4,12 +4,24 @@ import fi.vrk.xroad.xroad_catalog_lister.ListMembers;
 import fi.vrk.xroad.xroad_catalog_lister.ListMembersResponse;
 import fi.vrk.xroad.xroad_catalog_lister.MemberList;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.ws.WebServiceMessage;
+import org.springframework.ws.context.MessageContext;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+import org.springframework.ws.soap.SoapHeader;
+import org.springframework.ws.soap.SoapHeaderElement;
+import org.springframework.ws.soap.SoapMessage;
+import org.springframework.xml.transform.TransformerHelper;
 
+import javax.xml.namespace.QName;
+import javax.xml.transform.*;
+import javax.xml.transform.stream.StreamResult;
+import java.io.StringWriter;
 import java.time.LocalTime;
+import java.util.Iterator;
 
 @Endpoint
 public class ServiceEndpoint {
@@ -20,12 +32,47 @@ public class ServiceEndpoint {
 
 	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "ListMembers")
 	@ResponsePayload
-	public ListMembersResponse listMembers(@RequestPayload ListMembers request) {
+	public ListMembersResponse listMembers(@RequestPayload ListMembers request, MessageContext messageContext) throws
+			Exception {
 		ListMembersResponse response = new ListMembersResponse();
 		response.setMemberList(new MemberList());
 		response.getMemberList().getMembers().addAll(serviceRepository.listServices(LocalTime.now
 				()));
 
+
+		SoapHeader reqHeader = getSoapHeader(messageContext.getRequest());
+
+		SoapHeader respHeader = getSoapHeader(messageContext.getResponse());
+		transformHeaders(reqHeader, respHeader);
 		return response;
+	}
+
+	protected SoapHeader getSoapHeader(WebServiceMessage message) {
+		if (message instanceof SoapMessage) {
+			SoapMessage soapMessage = (SoapMessage) message;
+			return soapMessage.getEnvelope().getHeader();
+		}
+		else {
+			return null;
+		}
+	}
+
+	protected void transformHeaders(SoapHeader reqHeader, SoapHeader respHeader) throws TransformerException {
+		Source source = reqHeader.getSource();
+		Transformer transformer = createNonIndentingTransformer();
+		StringWriter writer = new StringWriter();
+		transformer.transform(source, respHeader.getResult());
+	}
+
+
+	private Transformer createNonIndentingTransformer() throws TransformerConfigurationException {
+		Transformer transformer = createTransformer();
+		transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+		transformer.setOutputProperty(OutputKeys.INDENT, "no");
+		return transformer;
+	}
+
+	protected final Transformer createTransformer() throws TransformerConfigurationException {
+		return new TransformerHelper().createTransformer();
 	}
 }
--- xroad-catalog-lister/src/main/java/fi/vrk/xroad/catalog/lister/WebServiceConfig.java
+++ xroad-catalog-lister/src/main/java/fi/vrk/xroad/catalog/lister/WebServiceConfig.java
@@ -7,12 +7,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.ws.config.annotation.EnableWs;
 import org.springframework.ws.config.annotation.WsConfigurerAdapter;
+import org.springframework.ws.server.EndpointInterceptor;
+import org.springframework.ws.soap.server.endpoint.interceptor.SoapEnvelopeLoggingInterceptor;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
-import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
 import org.springframework.ws.wsdl.wsdl11.SimpleWsdl11Definition;
 import org.springframework.ws.wsdl.wsdl11.Wsdl11Definition;
-import org.springframework.xml.xsd.SimpleXsdSchema;
-import org.springframework.xml.xsd.XsdSchema;
+
+import java.util.List;
 
 @EnableWs
 @Configuration
@@ -25,22 +26,17 @@ public class WebServiceConfig extends WsConfigurerAdapter {
 		return new ServletRegistrationBean(servlet, "/ws/*");
 	}
 
-	/*
-	@Bean(name = "services")
-	public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema servicesSchema) {
-		DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
-		wsdl11Definition.setPortTypeName("ServicesPort");
-		wsdl11Definition.setLocationUri("/ws");
-		wsdl11Definition.setTargetNamespace("http://xroad.vrk.fi/xroad-catalog-lister");
-		wsdl11Definition.setSchema(servicesSchema);
-		return wsdl11Definition;
-	}
-*/
 	@Bean(name = "services")
 	public Wsdl11Definition defaultWsdl11Definition() {
 		SimpleWsdl11Definition wsdl11Definition = new SimpleWsdl11Definition();
 		wsdl11Definition.setWsdl(new ClassPathResource("services.wsdl"));
 		return wsdl11Definition;
+	}
+
+	@Override
+	public void addInterceptors(List<EndpointInterceptor> interceptors) {
+
+		interceptors.add(new SoapEnvelopeLoggingInterceptor());
 	}
 
 
