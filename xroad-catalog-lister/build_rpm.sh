f8a77e8e27d4d8b2aa80825304a269e1da0a8620
Ilkka Seppälä <ilkka.seppala@gofore.com>
2016-02-26 09:46:20 +0200

    Fix Docker build

--- xroad-catalog-lister/build_rpm.sh
+++ xroad-catalog-lister/build_rpm.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-cd packages/xroad-catalog-lister/redhat
+cd workspace/xroad-catalog-lister/packages/xroad-catalog-lister/redhat
 rpmbuild --define "_topdir `pwd`" -ba SPECS/xroad-catalog-lister.spec
