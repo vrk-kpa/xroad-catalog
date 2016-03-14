c04bd8c1169855eee74edbbcd5b01970b76309cd
Janne Mattila <janne.mattila@gofore.com>
2016-03-14 16:27:26 +0200

    PVAYLADEV-290 refactored tests

--- xroad-catalog-persistence/src/main/java/fi/vrk/xroad/catalog/persistence/entity/ServiceId.java
+++ xroad-catalog-persistence/src/main/java/fi/vrk/xroad/catalog/persistence/entity/ServiceId.java
@@ -16,24 +16,4 @@ public class ServiceId {
         this.serviceCode = serviceCode;
         this.serviceVersion = serviceVersion;
     }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        ServiceId serviceId = (ServiceId) o;
-
-        if (serviceCode != null ? !serviceCode.equals(serviceId.serviceCode) : serviceId.serviceCode != null)
-            return false;
-        return serviceVersion != null ? serviceVersion.equals(serviceId.serviceVersion) : serviceId.serviceVersion == null;
-
-    }
-
-    @Override
-    public int hashCode() {
-        int result = serviceCode != null ? serviceCode.hashCode() : 0;
-        result = 31 * result + (serviceVersion != null ? serviceVersion.hashCode() : 0);
-        return result;
-    }
 }
