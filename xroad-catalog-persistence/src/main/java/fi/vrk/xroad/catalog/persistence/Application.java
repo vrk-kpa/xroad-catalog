4505d9cafa08dc0af08100a7d36c934a17a50e77
Sami Kallio <sami.kallio@gofore.com>
2016-05-13 15:30:41 +0300

    Removed empty private constructor

--- xroad-catalog-persistence/src/main/java/fi/vrk/xroad/catalog/persistence/Application.java
+++ xroad-catalog-persistence/src/main/java/fi/vrk/xroad/catalog/persistence/Application.java
@@ -22,14 +22,9 @@
  */
 package fi.vrk.xroad.catalog.persistence;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.orm.jpa.EntityScan;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
 @EntityScan(
@@ -37,10 +32,6 @@ import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 )
 @SpringBootApplication
 public class Application {
-
-    private Application() {
-        // Private empty constructor
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
