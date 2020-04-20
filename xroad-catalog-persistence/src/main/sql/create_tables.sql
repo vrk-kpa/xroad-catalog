17340192f4dff598134ff5619613d6538948641f
bertvi <bert.viikmae@gofore.com>
2020-04-20 14:39:10 +0300

    refacto db tables creation so that tables would be created every time if they do not exist

--- xroad-catalog-persistence/src/main/sql/create_tables.sql
+++ xroad-catalog-persistence/src/main/sql/create_tables.sql
@@ -9,228 +9,137 @@ SET standard_conforming_strings = on;
 SET check_function_bodies = false;
 SET client_min_messages = warning;
 
-
-
-CREATE TABLE member (
-    id bigint NOT NULL,
-    x_road_instance text NOT NULL,
-    member_class text NOT NULL,
-    member_code text NOT NULL,
-    name text NOT NULL,
-    created timestamp with time zone NOT NULL,
-    changed timestamp with time zone NOT NULL,
-    fetched timestamp with time zone NOT NULL,
-    removed timestamp with time zone
+CREATE TABLE IF NOT EXISTS member (
+    id BIGSERIAL PRIMARY KEY NOT NULL,
+    x_road_instance TEXT NOT NULL,
+    member_class TEXT NOT NULL,
+    member_code TEXT NOT NULL,
+    name TEXT NOT NULL,
+    created TIMESTAMP WITH TIME ZONE NOT NULL,
+    changed TIMESTAMP WITH TIME ZONE NOT NULL,
+    fetched TIMESTAMP WITH TIME ZONE NOT NULL,
+    removed TIMESTAMP WITH TIME ZONE
 );
 
-
-
-
-
-CREATE SEQUENCE member_id_seq
+CREATE SEQUENCE IF NOT EXISTS member_id_seq
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
     NO MAXVALUE
     CACHE 1;
-
 
 ALTER SEQUENCE member_id_seq OWNED BY member.id;
 
-
-
-CREATE TABLE service (
-    id bigint NOT NULL,
-    subsystem_id bigint NOT NULL,
-    service_code text NOT NULL,
-    service_version text,
-    created timestamp with time zone NOT NULL,
-    changed timestamp with time zone NOT NULL,
-    fetched timestamp with time zone NOT NULL,
-    removed timestamp with time zone
+CREATE TABLE IF NOT EXISTS subsystem (
+    id BIGSERIAL PRIMARY KEY NOT NULL,
+    member_id BIGSERIAL NOT NULL REFERENCES member(id),
+    subsystem_code TEXT NOT NULL,
+    created TIMESTAMP WITH TIME ZONE NOT NULL,
+    changed TIMESTAMP WITH TIME ZONE NOT NULL,
+    fetched TIMESTAMP WITH TIME ZONE NOT NULL,
+    removed TIMESTAMP WITH TIME ZONE
 );
 
-
-
-
-CREATE SEQUENCE service_id_seq
+CREATE SEQUENCE IF NOT EXISTS subsystem_id_seq
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
     NO MAXVALUE
     CACHE 1;
-
-
-ALTER SEQUENCE service_id_seq OWNED BY service.id;
-
-
-
-CREATE TABLE subsystem (
-    id bigint NOT NULL,
-    member_id bigint NOT NULL,
-    subsystem_code text NOT NULL,
-    created timestamp with time zone NOT NULL,
-    changed timestamp with time zone NOT NULL,
-    fetched timestamp with time zone NOT NULL,
-    removed timestamp with time zone
-);
-
-
-CREATE SEQUENCE subsystem_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
 
 ALTER SEQUENCE subsystem_id_seq OWNED BY subsystem.id;
 
-
-CREATE TABLE wsdl (
-    id bigint NOT NULL,
-    service_id bigint NOT NULL,
-    data text NOT NULL,
-    external_id text NOT NULL,
-    created timestamp with time zone NOT NULL,
-    changed timestamp with time zone NOT NULL,
-    fetched timestamp with time zone NOT NULL,
-    removed timestamp with time zone
+CREATE TABLE IF NOT EXISTS service (
+    id BIGSERIAL PRIMARY KEY NOT NULL,
+    subsystem_id BIGSERIAL NOT NULL REFERENCES subsystem(id),
+    service_code TEXT NOT NULL,
+    service_version TEXT,
+    created TIMESTAMP WITH TIME ZONE NOT NULL,
+    changed TIMESTAMP WITH TIME ZONE NOT NULL,
+    fetched TIMESTAMP WITH TIME ZONE NOT NULL,
+    removed TIMESTAMP WITH TIME ZONE
 );
 
-
-
-
-
-CREATE SEQUENCE wsdl_id_seq
+CREATE SEQUENCE IF NOT EXISTS service_id_seq
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
     NO MAXVALUE
     CACHE 1;
 
+ALTER SEQUENCE service_id_seq OWNED BY service.id;
 
+CREATE TABLE IF NOT EXISTS wsdl (
+    id BIGSERIAL PRIMARY KEY NOT NULL,
+    service_id BIGSERIAL NOT NULL REFERENCES service(id),
+    data TEXT NOT NULL,
+    external_id TEXT NOT NULL,
+    created TIMESTAMP WITH TIME ZONE NOT NULL,
+    changed TIMESTAMP WITH TIME ZONE NOT NULL,
+    fetched TIMESTAMP WITH TIME ZONE NOT NULL,
+    removed TIMESTAMP WITH TIME ZONE
+);
+
+CREATE SEQUENCE IF NOT EXISTS wsdl_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
 
 ALTER SEQUENCE wsdl_id_seq OWNED BY wsdl.id;
 
-CREATE SEQUENCE wsdl_external_id_seq
-START WITH 1
-INCREMENT BY 1
-NO MINVALUE
-NO MAXVALUE
-CACHE 1;
-
-
-
-ALTER SEQUENCE wsdl_external_id_seq OWNED BY wsdl.external_id;
-
-
-CREATE TABLE open_api (
-    id bigint NOT NULL,
-    service_id bigint NOT NULL,
-    data text NOT NULL,
-    external_id text NOT NULL,
-    created timestamp with time zone NOT NULL,
-    changed timestamp with time zone NOT NULL,
-    fetched timestamp with time zone NOT NULL,
-    removed timestamp with time zone
-);
-
-
-
-
-
-CREATE SEQUENCE openapi_id_seq
+CREATE SEQUENCE IF NOT EXISTS wsdl_external_id_seq
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
     NO MAXVALUE
     CACHE 1;
 
+ALTER SEQUENCE wsdl_external_id_seq OWNED BY wsdl.external_id;
 
+CREATE TABLE IF NOT EXISTS open_api (
+    id BIGSERIAL PRIMARY KEY NOT NULL,
+    service_id BIGSERIAL NOT NULL REFERENCES service(id),
+    data TEXT NOT NULL,
+    external_id TEXT NOT NULL,
+    created TIMESTAMP WITH TIME ZONE NOT NULL,
+    changed TIMESTAMP WITH TIME ZONE NOT NULL,
+    fetched TIMESTAMP WITH TIME ZONE NOT NULL,
+    removed TIMESTAMP WITH TIME ZONE
+);
 
-ALTER SEQUENCE openapi_id_seq OWNED BY open_api.id;
+CREATE SEQUENCE IF NOT EXISTS open_api_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
 
-CREATE SEQUENCE openapi_external_id_seq
-START WITH 1
-INCREMENT BY 1
-NO MINVALUE
-NO MAXVALUE
-CACHE 1;
+ALTER SEQUENCE open_api_id_seq OWNED BY open_api.id;
 
+CREATE SEQUENCE IF NOT EXISTS open_api_external_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
 
+ALTER SEQUENCE open_api_external_id_seq OWNED BY open_api.external_id;
 
-ALTER SEQUENCE openapi_external_id_seq OWNED BY open_api.external_id;
+CREATE UNIQUE INDEX IF NOT EXISTS idx_wsdl_external_id ON wsdl USING btree (external_id);
+CREATE UNIQUE INDEX IF NOT EXISTS idx_open_api_external_id ON open_api USING btree (external_id);
+CREATE UNIQUE INDEX IF NOT EXISTS idx_member_natural_keys ON member(member_code, member_class, x_road_instance);
+CREATE UNIQUE INDEX IF NOT EXISTS idx_service_unique_fields ON service(subsystem_id, service_code, service_version);
+CREATE UNIQUE INDEX IF NOT EXISTS idx_subsystem_unique_fields ON subsystem(member_id, subsystem_code);
 
-
-
-
-
-
-
-
-ALTER TABLE ONLY member
-    ADD CONSTRAINT primary_key_client PRIMARY KEY (id);
-
-
-
-ALTER TABLE ONLY service
-    ADD CONSTRAINT primary_key_service PRIMARY KEY (id);
-
-
-
-
-ALTER TABLE ONLY subsystem
-    ADD CONSTRAINT primary_key_subsystem PRIMARY KEY (id);
-
-
-
-ALTER TABLE ONLY wsdl
-    ADD CONSTRAINT primary_key_wsdl PRIMARY KEY (id);
-
-
-
-ALTER TABLE ONLY open_api
-    ADD CONSTRAINT primary_key_openapi PRIMARY KEY (id);
-
-
-
-CREATE UNIQUE INDEX idx_wsdl_external_id ON wsdl USING btree (external_id);
-CREATE UNIQUE INDEX idx_openapi_external_id ON open_api USING btree (external_id);
-CREATE UNIQUE INDEX idx_member_natural_keys ON member(member_code, member_class, x_road_instance);
-CREATE UNIQUE INDEX idx_service_unique_fields ON service(subsystem_id, service_code, service_version);
-CREATE UNIQUE INDEX idx_subsystem_unique_fields ON subsystem(member_id, subsystem_code);
-
-CREATE INDEX idx_wsdl_changed ON wsdl(changed);
-CREATE INDEX idx_openapi_changed ON open_api(changed);
-CREATE INDEX idx_service_changed ON service(changed);
-CREATE INDEX idx_subsystem_changed ON subsystem(changed);
-CREATE INDEX idx_member_changed ON member(changed);
-CREATE INDEX idx_wsdl_service_id ON wsdl(service_id);
-CREATE INDEX idx_openapi_service_id ON open_api(service_id);
-
-
-
-
-ALTER TABLE ONLY subsystem
-    ADD CONSTRAINT foreign_key_member FOREIGN KEY (member_id) REFERENCES member(id);
-
-
-
-ALTER TABLE ONLY wsdl
-    ADD CONSTRAINT foreign_key_service FOREIGN KEY (service_id) REFERENCES service(id);
-
-
-
-ALTER TABLE ONLY open_api
-    ADD CONSTRAINT foreign_key_service FOREIGN KEY (service_id) REFERENCES service(id);
-
-
-
-ALTER TABLE ONLY service
-    ADD CONSTRAINT foreign_key_subsystem FOREIGN KEY (subsystem_id) REFERENCES subsystem(id);
-
+CREATE INDEX IF NOT EXISTS idx_wsdl_changed ON wsdl(changed);
+CREATE INDEX IF NOT EXISTS idx_open_api_changed ON open_api(changed);
+CREATE INDEX IF NOT EXISTS idx_service_changed ON service(changed);
+CREATE INDEX IF NOT EXISTS idx_subsystem_changed ON subsystem(changed);
+CREATE INDEX IF NOT EXISTS idx_member_changed ON member(changed);
+CREATE INDEX IF NOT EXISTS idx_wsdl_service_id ON wsdl(service_id);
+CREATE INDEX IF NOT EXISTS idx_open_api_service_id ON open_api(service_id);
 
 ALTER TABLE member OWNER TO xroad_catalog;
 ALTER TABLE service OWNER TO xroad_catalog;
