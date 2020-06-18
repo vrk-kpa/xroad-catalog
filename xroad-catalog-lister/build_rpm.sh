116615176b7b1205d4182b0c949ed54c7e289689
bertvi <bert.viikmae@gofore.com>
2020-06-18 08:00:23 +0300

    update project version

--- xroad-catalog-lister/build_rpm.sh
+++ xroad-catalog-lister/build_rpm.sh
@@ -15,7 +15,7 @@ rm -rf ${ROOT}/RPMS/*
 
 
 rpmbuild \
-    --define "xroad_catalog_version 0.11.8" \
+    --define "xroad_catalog_version 0.11.9" \
     --define "rel $RELEASE" \
     --define "snapshot .$SNAPSHOT" \
     --define "_topdir $ROOT" \
